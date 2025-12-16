938b421ae390b5ee432ca597566d21b6e7de5304
Teja Madu <teja.madu@gmail.com>
2025-12-16 09:59:15 -0600

    Building Game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,10 +10,10 @@ function GameCard({game}: GameCardProps) {
 
     return (
         
-        <Card.Root borderRadius={10}>
-            <Image src={game.background_image} alt={game.name} />
+        <Card.Root borderRadius={10} overflow="hidden" h="400px">
+            <Image src={game.background_image} alt={game.name} h="200px" objectFit="cover"/>
             <Card.Body>
-                <Heading>{game.name}</Heading>
+                <Heading size="2xl">{game.name}</Heading>
             </Card.Body>
             <Card.Footer>
                 <Text>{game.platforms.map(({name}) => name).join(', ')}</Text>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,7 +9,7 @@ function GameGrid() {
     return (
         <>
         {error && <Text>{error}</Text>}
-        <SimpleGrid columns={3} >
+        <SimpleGrid columns={{sm:1,md: 2, lg: 3, xl: 5}} gap={10} padding={10}>
             {games.map(game => (
                 <GameCard game={game} key={game.id} />
             ))}
